5ac6c6bef95b2cd66cd2bc10c5ae9b291a2617e2
piyushagarwal62 <piyush6262@gmail.com>
2020-03-03 18:45:23 +0530

    Added code for implementation of collections classes

---
+++ src/collections/Student.java
@@ -0,0 +1,37 @@
+package collections;
+
+public class Student {
+	
+	int id;
+	String name;
+	String location;
+	public Student(int id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	@Override
+	public String toString() {
+		return id + " " + name + " " + location;
+	}
+}
