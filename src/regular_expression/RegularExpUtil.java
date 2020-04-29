6458cd0cd9c2ab4d4a1d64d75b52a7a9b6198e92
piyushagarwal62 <piyush6262@gmail.com>
2020-04-29 15:05:50 +0530

    Added modification to existing code

--- src/regular_expression/RegularExpUtil.java
+++ src/regular_expression/RegularExpUtil.java
@@ -1,15 +1,19 @@
 package regular_expression;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author piyush.agarwal
- * Utility class for Regular Expression
+ * @author piyush.agarwal Utility class for Regular Expression
  *
  */
 public class RegularExpUtil {
 
 	public static void test(String regex, String input) {
+		test(regex, input, 0);
+	}
+
+	public static void test(String regex, String input, int flag) {
 		System.out.println("--------------------");
 		System.out.printf("Regex: '%s', Input: '%s'\n", regex, input);
 		testFind(regex, input);
@@ -35,8 +39,7 @@ public class RegularExpUtil {
 		while (matcher.find()) {
 			String matchedValue = matcher.group();
 
-			System.out.printf("Matched  %s---%s---'%s'%n", matcher.start(),
-					matcher.end(), matchedValue);
+			System.out.printf("Matched  %s---%s---'%s'%n", matcher.start(), matcher.end(), matchedValue);
 			showGroupInformation(matcher);
 		}
 	}
@@ -48,23 +51,21 @@ public class RegularExpUtil {
 		while (matcher.find()) {
 			String matchedValue = matcher.group();
 
-			System.out.printf("Matched (Group zero) %s---%s---'%s'%n", matcher.start(),
-					matcher.end(), matchedValue);
+			System.out.printf("Matched (Group zero) %s---%s---'%s'%n", matcher.start(), matcher.end(), matchedValue);
 			showGroupInformation(matcher);
 		}
 	}
-	
+
 	private static void testReplaceAll(String regex, String input, String replaceString) {
-        System.out.printf("Lets replace all matches with xyz=> '%s'%n",
-                Pattern.compile(regex).matcher(input).replaceAll(replaceString));
-    }
-	
+		System.out.printf("Lets replace all matches with xyz=> '%s'%n",
+				Pattern.compile(regex).matcher(input).replaceAll(replaceString));
+	}
+
 	public static void showGroupInformation(Matcher matcher) {
 
-        for (int i = 1; i <= matcher.groupCount(); i++) {
-            System.out.printf("Group number: %s, " +
-                            "Group matched String: '%s',  start: %s, end: %s%n",
-                    i, matcher.group(i), matcher.start(i), matcher.end(i));
-        }
-    }
+		for (int i = 1; i <= matcher.groupCount(); i++) {
+			System.out.printf("Group number: %s, " + "Group matched String: '%s',  start: %s, end: %s%n", i,
+					matcher.group(i), matcher.start(i), matcher.end(i));
+		}
+	}
 }
